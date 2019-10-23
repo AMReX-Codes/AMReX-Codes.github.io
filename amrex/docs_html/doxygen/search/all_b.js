ca7c2d2a0a65693bf1775b2ad26c393139b1e997
Travis CI <atmyers@lbl.gov>
2019-10-23 22:24:18 +0000

    Deploy to GitHub Pages: 58dbb83dde3819c47889fcc87c66e3586b65f675

--- amrex/docs_html/doxygen/search/all_b.js
+++ amrex/docs_html/doxygen/search/all_b.js
@@ -12,7 +12,7 @@ var searchData=
   ['knapsackdoit',['KnapSackDoIt',['../classamrex_1_1DistributionMapping.html#a43db53d49a2e06ff06fa40a7e3d292a6',1,'amrex::DistributionMapping']]],
   ['knapsackprocessormap',['KnapSackProcessorMap',['../classamrex_1_1DistributionMapping.html#ab1d611f13754052ecd7ac9363042516f',1,'amrex::DistributionMapping::KnapSackProcessorMap(const std::vector&lt; long &gt; &amp;wgts, int nprocs, Real *efficiency=0, bool do_full_knapsack=true, int nmax=std::numeric_limits&lt; int &gt;::max())'],['../classamrex_1_1DistributionMapping.html#adee3630696a974eb1b9c22046f0dc653',1,'amrex::DistributionMapping::KnapSackProcessorMap(const BoxArray &amp;boxes, int nprocs)']]],
   ['knoth_5fwolke_5f3_5f3',['knoth_wolke_3_3',['../namespacefarkode__mod.html#acc9dea8111571ab11d16aa5294d14dc0',1,'farkode_mod']]],
-  ['kstride',['kstride',['../structamrex_1_1Array4.html#a4adbc6fbedab06dc579764520c4ec9e6',1,'amrex::Array4::kstride()'],['../structamrex_1_1FabView.html#aa88c0689af62b9174fa955acf31d614e',1,'amrex::FabView::kstride()']]],
+  ['kstride',['kstride',['../structamrex_1_1Array4.html#a4adbc6fbedab06dc579764520c4ec9e6',1,'amrex::Array4']]],
   ['kt_5fsize',['kt_size',['../classamrex_1_1BaseUmap.html#ad620fcee1e1d1d1dafaad74468416138',1,'amrex::BaseUmap']]],
   ['kvaerno_5f4_5f2_5f3',['kvaerno_4_2_3',['../namespacefarkode__mod.html#a031fca9ae6beee6a85f7b2157b1dcd39',1,'farkode_mod']]],
   ['kvaerno_5f5_5f3_5f4',['kvaerno_5_3_4',['../namespacefarkode__mod.html#a652e2315fd33b2100d0f4cfabf826d88',1,'farkode_mod']]],
