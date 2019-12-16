cb0627849d4072c4c9097406575b20e313352290
Travis CI <atmyers@lbl.gov>
2019-12-16 18:43:46 +0000

    Deploy to GitHub Pages: b3ad48e9769a1c56277cb9f748a0016bc20a03da

--- amrex/tutorials_html/_static/documentation_options.js
+++ amrex/tutorials_html/_static/documentation_options.js
@@ -3,6 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '19.01-dev',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
