d1a758d97fd7318fd9340f349e8546b8d6fc59f8
atmyers <atmyers2@gmail.com>
2020-12-07 18:35:54 +0000

    Deploying to main from  @ 6fb23a385bf2fa0bc2e3b64a24e4940b4c0ea16d 🚀

--- amrex/docs_html/doxygen/search/functions_b.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['key',['key',['../classamrex_1_1BaseUmap.html#a6cf2ab53ed1819eaf29fe02788293941',1,'amrex::BaseUmap']]],
-  ['keytableptr',['keyTablePtr',['../classamrex_1_1BaseUmap.html#a338b21ba6abefea8cab9cb9f257929fb',1,'amrex::BaseUmap::keyTablePtr()'],['../classamrex_1_1BaseUmap.html#a1a4952f4e66416d51654e2263dadfa7f',1,'amrex::BaseUmap::keyTablePtr(int n=0) const ']]],
-  ['knapsack',['knapsack',['../namespaceamrex.html#a7c149cd13c0bd6bcd97464f0406a600b',1,'amrex']]],
-  ['knapsackdoit',['KnapSackDoIt',['../classamrex_1_1DistributionMapping.html#a1f6ec86cb567ef3c761c2cf693aa870d',1,'amrex::DistributionMapping']]],
-  ['knapsackprocessormap',['KnapSackProcessorMap',['../classamrex_1_1DistributionMapping.html#af41949391775da61f74853fc6a16b686',1,'amrex::DistributionMapping::KnapSackProcessorMap(const std::vector&lt; Long &gt; &amp;wgts, int nprocs, Real *efficiency=0, bool do_full_knapsack=true, int nmax=std::numeric_limits&lt; int &gt;::max(), bool sort=true)'],['../classamrex_1_1DistributionMapping.html#adee3630696a974eb1b9c22046f0dc653',1,'amrex::DistributionMapping::KnapSackProcessorMap(const BoxArray &amp;boxes, int nprocs)']]]
-];
--- amrex/docs_html/doxygen/search/typedefs_12.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['userfillbox',['UserFillBox',['../namespaceamrex.html#ac0684d4e484a2094882fcb104257c6c2',1,'amrex']]]
-];
