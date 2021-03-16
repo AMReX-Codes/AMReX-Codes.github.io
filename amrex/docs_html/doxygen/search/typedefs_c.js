c08cc7f5d014b7a1d92d6f729e36a83dbfc2ecb3
atmyers <atmyers2@gmail.com>
2021-03-16 22:05:47 +0000

    Deploying to main from  @ c28d91c2ee07ad6f970eb10bbc14d00af6adab4a 🚀

--- amrex/docs_html/doxygen/search/typedefs_c.js
+++ amrex/docs_html/doxygen/search/typedefs_c.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['negation_13958',['Negation',['../namespaceamrex.html#a5733e0ae95b529a0276f6bea8ecbedf9',1,'amrex']]],
+  ['negation_13958',['Negation',['../namespaceamrex.html#a3d7af06ef9ce2e0a696e5c73f3e460aa',1,'amrex']]],
   ['neighborcommmap_13959',['NeighborCommMap',['../classamrex_1_1NeighborParticleContainer.html#ae77c84d55fb9e147fc49a60bd44d42d6',1,'amrex::NeighborParticleContainer']]],
   ['nl_13960',['NL',['../classamrex_1_1CArena.html#a9e8adf2bef58badafbefa442d69db64b',1,'amrex::CArena']]]
 ];
