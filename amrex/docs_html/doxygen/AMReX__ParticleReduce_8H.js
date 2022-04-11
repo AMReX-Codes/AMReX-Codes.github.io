0fd2b30b9303cbc7ddd63e8feced29b23f2177a5
atmyers <atmyers2@gmail.com>
2022-04-11 16:04:56 +0000

    Deploying to main from  @ 872ce5692775fd0d659db001b838328806f5ae90 🚀

--- amrex/docs_html/doxygen/AMReX__ParticleReduce_8H.js
+++ amrex/docs_html/doxygen/AMReX__ParticleReduce_8H.js
@@ -9,13 +9,13 @@ var AMReX__ParticleReduce_8H =
     [ "ReduceLogicalOr", "AMReX__ParticleReduce_8H.html#a43f13583043013b5751afe1e193cc8b5", null ],
     [ "ReduceLogicalOr", "AMReX__ParticleReduce_8H.html#a18f76938d120a4d9891cc99e67cab771", null ],
     [ "ReduceLogicalOr", "AMReX__ParticleReduce_8H.html#a4045ba476918bce64dc92d94e4cda26d", null ],
-    [ "ReduceMax", "AMReX__ParticleReduce_8H.html#a7ea00de6fd7be3ddb1b4f841295cac21", null ],
-    [ "ReduceMax", "AMReX__ParticleReduce_8H.html#af55bd59d2c594a63ae6bb0136596fb9b", null ],
-    [ "ReduceMax", "AMReX__ParticleReduce_8H.html#aa9bf7aa5f979cd910b5b270070cac8b1", null ],
-    [ "ReduceMin", "AMReX__ParticleReduce_8H.html#a3aa271dcaabdcc69a172b422fdc60349", null ],
-    [ "ReduceMin", "AMReX__ParticleReduce_8H.html#aab38aa5f542e67c840d6e9c6d30d586a", null ],
-    [ "ReduceMin", "AMReX__ParticleReduce_8H.html#ab8f47432b3e35fe46b1b89def917916e", null ],
-    [ "ReduceSum", "AMReX__ParticleReduce_8H.html#a8f1971a46d37a951bb0ef8b189f8987f", null ],
-    [ "ReduceSum", "AMReX__ParticleReduce_8H.html#a9600b570271756f90a1c8b3c7e05a9a1", null ],
-    [ "ReduceSum", "AMReX__ParticleReduce_8H.html#a234b29ca2ab42dc4b7d3713800d82a4e", null ]
+    [ "ReduceMax", "AMReX__ParticleReduce_8H.html#afcc19ed59f5327a67f329b7745af5802", null ],
+    [ "ReduceMax", "AMReX__ParticleReduce_8H.html#a372810a65569e56565a9bb33254184bd", null ],
+    [ "ReduceMax", "AMReX__ParticleReduce_8H.html#a2a143a183f01010f7f57ca6025fc0b81", null ],
+    [ "ReduceMin", "AMReX__ParticleReduce_8H.html#a23f8188bafacc115d2cfd1dfcda0db65", null ],
+    [ "ReduceMin", "AMReX__ParticleReduce_8H.html#af6e2c75c0f4c085c615019cbd2f7fe94", null ],
+    [ "ReduceMin", "AMReX__ParticleReduce_8H.html#a8d2f558353675dae0aa185f137d8e0ea", null ],
+    [ "ReduceSum", "AMReX__ParticleReduce_8H.html#a4a0d7f39455f1fad5de147aa8cbf45a4", null ],
+    [ "ReduceSum", "AMReX__ParticleReduce_8H.html#afe943e0bac4c931ba74fcfb7e0fceb64", null ],
+    [ "ReduceSum", "AMReX__ParticleReduce_8H.html#a6a4e13460f583730884202aa24ba165f", null ]
 ];
--- amrex/docs_html/doxygen/AMReX__ParticleUtil_8H.js
+++ amrex/docs_html/doxygen/AMReX__ParticleUtil_8H.js
@@ -1,8 +1,10 @@
 var AMReX__ParticleUtil_8H =
 [
     [ "GetParticleBin", "structamrex_1_1GetParticleBin.html", "structamrex_1_1GetParticleBin" ],
-    [ "call_f", "AMReX__ParticleUtil_8H.html#aee2d2644261511e4ae19434dfed4df1f", null ],
-    [ "call_f", "AMReX__ParticleUtil_8H.html#a15853ec16ab60f72f428df7ca6a2c2cf", null ],
+    [ "call_f", "AMReX__ParticleUtil_8H.html#a911e0c6ceae626e1271a245642d1f7f4", null ],
+    [ "call_f", "AMReX__ParticleUtil_8H.html#a750d9d77d407472797e4a604f6ec7a61", null ],
+    [ "call_f", "AMReX__ParticleUtil_8H.html#adf80e5e4411bb1b45deb6b84f073b719", null ],
+    [ "call_f", "AMReX__ParticleUtil_8H.html#a490dfd7a0f1f5a14f1fedbdb60c7af38", null ],
     [ "call_f", "AMReX__ParticleUtil_8H.html#aeefdb393afef57732439f230e1b53578", null ],
     [ "clearEmptyEntries", "AMReX__ParticleUtil_8H.html#aece8b1e2716e84a5261e988b9e9b1ef2", null ],
     [ "computeNeighborProcs", "AMReX__ParticleUtil_8H.html#ac6d35da48f520c8ae3f429aee03601c9", null ],
@@ -19,10 +21,8 @@ var AMReX__ParticleUtil_8H =
     [ "numParticlesOutOfRange", "AMReX__ParticleUtil_8H.html#a16b2ad610aeda065996254dcb64e1210", null ],
     [ "numTilesInBox", "AMReX__ParticleUtil_8H.html#a77b016483c8263c1a67e93d79d31da4c", null ],
     [ "partitionParticlesByDest", "AMReX__ParticleUtil_8H.html#ae0112b869a3cebcbaa5160751f6768ae", null ],
-    [ "NAI", "AMReX__ParticleUtil_8H.html#a50df1f8476310d2f48746df9ae8a908d", null ],
-    [ "NAR", "AMReX__ParticleUtil_8H.html#a9019d48c537279e526835b2e17bddb66", null ],
-    [ "NSI", "AMReX__ParticleUtil_8H.html#a568ffbac9443a55d182eed8d76d5472c", null ],
-    [ "NSR", "AMReX__ParticleUtil_8H.html#a90b317f350cc94cff5fb753a9389ad4b", null ],
-    [ "RandomEngine", "AMReX__ParticleUtil_8H.html#ae1c3a3e9f6d98db25c5fb304fef91d6a", null ],
-    [ "T", "AMReX__ParticleUtil_8H.html#a096471cf807563c45779ef7a20181fbe", null ]
+    [ "NAR", "AMReX__ParticleUtil_8H.html#ae8a815a03dd199d4f0a63a1ea9618ce8", null ],
+    [ "NSI", "AMReX__ParticleUtil_8H.html#af1a0333c4222e4b0817845fdef5f20bd", null ],
+    [ "NSR", "AMReX__ParticleUtil_8H.html#a56c54a0833b61d62720fc2ea149afaea", null ],
+    [ "RandomEngine", "AMReX__ParticleUtil_8H.html#ae1c3a3e9f6d98db25c5fb304fef91d6a", null ]
 ];
