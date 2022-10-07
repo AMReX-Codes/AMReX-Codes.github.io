d9087ddb527328261ddd4f0d620c2159f9584009
cgilet <cgilet@users.noreply.github.com>
2022-10-07 15:55:44 +0000

    Deploying to main from  @ dd9c0592df449fd6a3ffe569bb0882b2bf7b0ea6 🚀

--- amrex-hydro/Doxygen/html/hydro__utils_8H.js
+++ amrex-hydro/Doxygen/html/hydro__utils_8H.js
@@ -2,7 +2,6 @@ var hydro__utils_8H =
 [
     [ "ComputeConvectiveTerm", "hydro__utils_8H.html#a9d491a26b6816bc1137fcc051ab78f56", null ],
     [ "ComputeDivergence", "hydro__utils_8H.html#a0f59801cda64952e4b63910b1f42c455", null ],
-    [ "ComputeEdgeState", "hydro__utils_8H.html#a541936cf4a3b5bcb586045ad47f37b77", null ],
     [ "ComputeFluxes", "hydro__utils_8H.html#ab70f040557a658e70ba076c9d105bab7", null ],
     [ "ComputeFluxesOnBoxFromState", "hydro__utils_8H.html#a8d3b669aa84b8b43a44e4a9fbf957c7f", null ],
     [ "ComputeFluxesOnBoxFromState", "hydro__utils_8H.html#a5c512411293167e507a418b15ec79e74", null ],
