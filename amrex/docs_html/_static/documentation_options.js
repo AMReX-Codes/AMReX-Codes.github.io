f454eb749e7ee5adc22ed368b1614b9786198d74
WeiqunZhang <WeiqunZhang@lbl.gov>
2024-06-01 19:47:03 +0000

    Deploying to main from @ AMReX-Codes/amrex@f141191fc73cb22ee649f89569c1778fc910d1fb 🚀

--- amrex/docs_html/_static/documentation_options.js
+++ amrex/docs_html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '24.06-dev',
+    VERSION: '24.07-dev',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
